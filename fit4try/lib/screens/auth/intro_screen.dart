0975ac93356b604bb8cc6849d09be3b5da2ca686
yaren çolpan <yarenncol8@gmail.com>
2024-06-25 13:53:56 +0300

    intro (onbording)kodları eklendi
    intro_screen ve intro_splash_screen içine sırasıyla kendi projemde yazdığım  main ve onboarding kısmını ekledim

--- fit4try/lib/screens/auth/intro_screen.dart
+++ fit4try/lib/screens/auth/intro_screen.dart
@@ -1,1 +1,84 @@
 //statlesswidget
+//Burada yarenin intro için yaptığı projenin main kısmı var 
+
+import 'package:fit4try/screens/auth/intro_splash_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:smooth_page_indicator/smooth_page_indicator.dart';    //bunu ayrıca pubspec.yaml ekledim 
+
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final PageController _pageController = PageController();
+
+  final List<Widget> _screens = const [
+    Screen1(),
+    Screen2(),
+    Screen3(),
+    Screen4(),
+    Screen5(),
+  ];
+
+  @override
+  void dispose() {
+    _pageController.dispose();
+    super.dispose();
+  }
+
+//aşağıda bu sayfanın altında sayfalar kaydıkça kayan gösterge kısmı var
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Stack(
+        children: [
+          PageView(
+            controller: _pageController,
+            children: _screens,
+          ),
+          Positioned(
+            bottom: 30,
+            left: 0,
+            right: 0,
+            child: Center(
+              child: SmoothPageIndicator(
+                controller: _pageController,
+                count: 5,
+                effect: WormEffect(
+                  dotHeight: 8,
+                  dotWidth: 8,
+                  activeDotColor: Colors.white,
+                  dotColor: Colors.white.withOpacity(0.5),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
--- fit4try/lib/screens/auth/intro_splash_screen.dart
+++ fit4try/lib/screens/auth/intro_splash_screen.dart
@@ -1,1 +1,363 @@
-//5 screen burayass
+//burada intro için oluşturduğum 5 sayfanın görüntüsü var 
+
+import 'package:fit4try/constants/fonts.dart';
+import 'package:flutter/material.dart';
+
+
+class Screen1 extends StatelessWidget {
+  const Screen1({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Stack(
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(bottom: 260),
+            child: Center(
+              child: Image.asset("assets/images/image1.png"),
+            ),
+          ),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Container(
+              width: MediaQuery.of(context).size.width,
+              height: MediaQuery.of(context).size.height / 2.5,
+              decoration: const BoxDecoration(
+                color: AppColors.primaryColor5,
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(30),
+                  topRight: Radius.circular(30),
+                ),
+              ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  const Padding(
+                    padding: EdgeInsets.only(top: 30),
+                    child: Text(
+                      "Sanal Kıyafet Deneme",
+                      style: TextStyle(
+                        color:  AppColors.backgroundColor2,
+                        fontSize: 20,
+                        fontWeight: FontWeight.w700,
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 30, left: 20, right: 20),
+                    child: Text(
+                      "Kullanıcılar, stilistlerin önerdiği kıyafetleri veya kendi seçtikleri kıyafetleri mobil uygulama üzerinden sanal olarak deneyebilirler.",
+                      style: TextStyle(
+                        color: AppColors.backgroundColor2.withOpacity(0.6),
+                        fontSize: 16,
+                        fontWeight: FontWeight.w400,
+                      ),
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class Screen2 extends StatelessWidget {
+  const Screen2({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Stack(
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(bottom: 260),
+            child: Center(
+              child: Image.asset("assets/images/image2.png"),
+            ),
+          ),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Container(
+              width: MediaQuery.of(context).size.width,
+              height: MediaQuery.of(context).size.height / 2.5,
+              decoration: const BoxDecoration(
+                color: AppColors.primaryColor5,
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(30),
+                  topRight: Radius.circular(30),
+                ),
+              ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  const Padding(
+                    padding: EdgeInsets.only(top: 30),
+                    child: Text(
+                      "Kendi Kıyafetlerini Tasarla",
+                      style: TextStyle(
+                        color: AppColors.backgroundColor2,
+                        fontSize: 20,
+                        fontWeight: FontWeight.w700,
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 30, left: 20, right: 20),
+                    child: Text(
+                      "Kullanıcılar, kendi kıyafetlerini çizerek veya tasarlayarak dolabına ekleyebilir hatta tasarımlarını paylaşabilir.",
+                      style: TextStyle(
+                        color: AppColors.backgroundColor2.withOpacity(0.6),
+                        fontSize: 16,
+                        fontWeight: FontWeight.w400,
+                      ),
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class Screen3 extends StatelessWidget {
+  const Screen3({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Stack(
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(bottom: 260),
+            child: Center(
+              child: Image.asset("assets/images/image3.png"),
+            ),
+          ),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Container(
+              width: MediaQuery.of(context).size.width,
+              height: MediaQuery.of(context).size.height / 2.5,
+              decoration: const BoxDecoration(
+                color: AppColors.primaryColor5,
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(30),
+                  topRight: Radius.circular(30),
+                ),
+              ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  const Padding(
+                    padding: EdgeInsets.only(top: 30, left: 60, right: 40),
+                    child: Text(
+                      "Tarzını Stil Danışmanına veya Yapay Zeka’ya Danış",
+                      style: TextStyle(
+                        color: AppColors.backgroundColor2,
+                        fontSize: 20,
+                        fontWeight: FontWeight.w700,
+                      ),
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 30, left: 20, right: 20),
+                    child: Text(
+                      "Kullanıcılar, kendi kıyafetlerini çizerek veya tasarlayarak dolabına ekleyebilir hatta tasarımlarını paylaşabilirsin",
+                      style: TextStyle(
+                        color: AppColors.backgroundColor2.withOpacity(0.6),
+                        fontSize: 16,
+                        fontWeight: FontWeight.w400,
+                      ),
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class Screen4 extends StatelessWidget {
+  const Screen4({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Stack(
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(bottom: 260),
+            child: Center(
+              child: Image.asset("assets/images/image4.png"),
+            ),
+          ),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Container(
+              width: MediaQuery.of(context).size.width,
+              height: MediaQuery.of(context).size.height / 2.5,
+              decoration: const BoxDecoration(
+                color: AppColors.primaryColor5,
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(30),
+                  topRight: Radius.circular(30),
+                ),
+              ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  const Padding(
+                    padding: EdgeInsets.only(top: 30),
+                    child: Text(
+                      "Topluluk ve Sosyal Paylaşım",
+                      style: TextStyle(
+                        color: AppColors.backgroundColor2,
+                        fontSize: 20,
+                        fontWeight: FontWeight.w700,
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 30, left: 20, right: 20),
+                    child: Text(
+                      "Kullanıcılar, tasarımlarını toplulukla paylaşabilir, beğeni ve geri bildirim alabilirler.",
+                      style: TextStyle(
+                        color: AppColors.backgroundColor2.withOpacity(0.6),
+                        fontSize: 16,
+                        fontWeight: FontWeight.w400,
+                      ),
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+
+class Screen5 extends StatelessWidget {
+  const Screen5({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Stack(
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(bottom: 260),
+            child: Center(
+              child: Image.asset("assets/images/image5.png"),
+            ),
+          ),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Container(
+              width: MediaQuery.of(context).size.width,
+              height: MediaQuery.of(context).size.height / 2.5,
+              decoration: const BoxDecoration(
+                color: AppColors.primaryColor5,
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(30),
+                  topRight: Radius.circular(30),
+                ),
+              ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  const Padding(
+                    padding: EdgeInsets.only(top: 30),
+                    child: Text(
+                      "Ödül Sistemi",
+                      style: TextStyle(
+                        color: AppColors.backgroundColor2,
+                        fontSize: 20,
+                        fontWeight: FontWeight.w700,
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 30, left: 20, right: 20),
+                    child: Text(
+                      "En çok kıyafet tarzı sunan kullanıcıların yanısıra uygulama içerisinde sende stilist olabilir hatta çeşitli ödüller kazanabilirsin.",
+                      style: TextStyle(
+                        color: AppColors.backgroundColor2.withOpacity(0.6),
+                        fontSize: 16,
+                        fontWeight: FontWeight.w400,
+                      ),
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+                  const Spacer(),
+                  Padding(
+                    padding: const EdgeInsets.only(bottom: 50),
+                    child: ElevatedButton(
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(builder: (context) => const NextScreen()), // Replace NextScreen with your next screen's class
+                        );
+                      },
+                      style: ElevatedButton.styleFrom(
+                        
+                        iconColor: Colors.white, // button color
+                        backgroundColor: AppColors.backgroundColor2, // text color
+                        padding: const EdgeInsets.symmetric(horizontal: 50, vertical: 15),
+                        
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(30),
+                        ),
+                      ),
+                      child: const Text(
+                        "Devam Et",
+                        style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.w700,
+                          color: AppColors.primaryColor5,
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class NextScreen extends StatelessWidget {
+  const NextScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      // Your NextScreen implementation
+    );
+  }
+}
