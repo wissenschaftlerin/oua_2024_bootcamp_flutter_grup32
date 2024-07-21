01e492537148716f35c5e35f249911979f679517
Zehranurbas <030121017@std.izu.edu.tr>
2024-07-21 13:58:01 +0300

    dil seçenekleri ayarları sayfası kodları eklendi

---
+++ fit4try/lib/screens/settings/language_options/language_selection_page_screen.dart
@@ -0,0 +1,138 @@
+import 'package:flutter/material.dart';
+
+
+class LanguageSelectionPage extends StatefulWidget {
+  @override
+  _LanguageSelectionPageState createState() => _LanguageSelectionPageState();
+}
+
+class _LanguageSelectionPageState extends State<LanguageSelectionPage> {
+  String _selectedLanguage = 'Türkçe';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(70.0), // AppBar'ın yüksekliğini ayarlayın
+        child: AppBar(
+          centerTitle: true, // Başlığı ortalayın
+          title: Padding(
+            padding: const EdgeInsets.only(top: 24.0),
+            child: Text('Dil Seçenekleri'),
+          ),
+          leading: IconButton(
+            icon: Image.asset('assets/icon/back.png'),
+            onPressed: () {
+              // Geri butonuna basılınca yapılacak işlemler
+            },
+          ),
+        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Divider(thickness: 1), // İnce çizgi
+            ListTile(
+              title: Text('English'),
+              leading: Radio<String>(
+                value: 'English',
+                groupValue: _selectedLanguage,
+                onChanged: (String? value) {
+                  setState(() {
+                    _selectedLanguage = value!;
+                  });
+                },
+              ),
+            ),
+            ListTile(
+              title: Text('Türkçe'),
+              leading: Radio<String>(
+                value: 'Türkçe',
+                groupValue: _selectedLanguage,
+                onChanged: (String? value) {
+                  setState(() {
+                    _selectedLanguage = value!;
+                  });
+                },
+              ),
+            ),
+            Spacer(),
+            Center(
+              child: ElevatedButton(
+                onPressed: _selectedLanguage.isNotEmpty ? () {
+                  // Kaydet butonuna basılınca yapılacak işlemler
+                } : null,
+                child: Container(
+                  width: double.infinity,
+                  padding: EdgeInsets.symmetric(vertical: 16),
+                  alignment: Alignment.center,
+                  child: Text(
+                    'Kaydet',
+                    style: TextStyle(
+                      fontSize: 16,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: _selectedLanguage == 'Türkçe'
+                      ? Color(0xFFC1AFCF) // Türkçe seçildiğinde buton rengi
+                      : Color(0xFF9F49DF), // İngilizce seçildiğinde buton rengi
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed, // İkonların hareket etmesini istersek bu satırı kaldırırız
+        items: [
+          BottomNavigationBarItem(
+            icon: Image.asset(
+              'assets/icon/home.png',
+              width: 36,
+              height: 36,
+            ),
+            label: '',
+          ),
+          BottomNavigationBarItem(
+            icon: Image.asset(
+              'assets/icon/community.png',
+              width: 36,
+              height: 36,
+            ),
+            label: '',
+          ),
+          BottomNavigationBarItem(
+            icon: Image.asset(
+              'assets/icon/picture.png',
+              width: 36,
+              height: 36,
+            ),
+            label: '',
+          ),
+          BottomNavigationBarItem(
+            icon: Image.asset(
+              'assets/icon/aı.png',
+              width: 36,
+              height: 36,
+            ),
+            label: '',
+          ),
+          BottomNavigationBarItem(
+            icon: CircleAvatar(
+              radius: 12,
+              backgroundImage: AssetImage('assets/icon/foto.png'),
+            ),
+            label: '',
+          ),
+        ],
+      ),
+    );
+  }
+}
